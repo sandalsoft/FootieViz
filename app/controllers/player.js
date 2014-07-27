51c84e6f5ebb4ed6beedede1da4436d2ab2de382
Eric Nelson <eric@sandalsoft.com>
2014-07-27 09:55:55 -0500

    - added poopulat player

--- app/controllers/player.js
+++ app/controllers/player.js
@@ -4,4 +4,10 @@ export default Ember.ObjectController.extend({
   needs: "players",
   totalPoints: Ember.computed.alias('controllers.players.totalPoints'),
   totalGoals: Ember.computed.alias('controllers.players.totalGoals'),
+  allPlayers: Ember.computed.alias('controllers.players.model'),
+
+  mostPopularPlayer: function() {
+    var orderedAllPlayers = this.get('allPlayers').sortBy('selected_by');
+    return orderedAllPlayers[orderedAllPlayers.length -1];
+    }.property('allPlayers'),
 });
