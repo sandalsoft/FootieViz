057897e174cc55e4b234a42ef7cb8cd884d6b7a6
Eric Nelson <eric@sandalsoft.com>
2014-07-31 09:22:07 -0400

    - added fixture adapter for future use

---
+++ app/adapters/_fixture.js
@@ -0,0 +1,16 @@
+import DS from 'ember-data';
+import ajax from 'ic-ajax';
+
+export default DS.RESTAdapter.extend({
+  host: window.FootieVizENV.HOST,
+  namespace: window.FootieVizENV.NAMESPACE,
+
+  defaultSerializer: '-default',
+
+ find: function(player_id){
+  var id = "204";
+  window.console.log('obj: ' + id);
+  return ajax(this.get('host') + '/' + this.get('namespace') + '/' + 'fixtures?filter[where][player_id]=' + id);
+  }
+
+});
